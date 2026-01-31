a349b5462d0fa77965e64b0520dca275920c0057
carlosesconav <carlosesconav@gmail.com>
2026-01-30 19:48:20 -0500

    feat: delete node moduies

--- pay-process-service/dist/domain/constants/transactions.status.enum.js
+++
@@ -1,6 +0,0 @@
-export var TransactionStatusEnum;
-(function (TransactionStatusEnum) {
-    TransactionStatusEnum["SUCCESS"] = "SUCCESS";
-    TransactionStatusEnum["PENDING"] = "PENDING";
-    TransactionStatusEnum["REJECTED"] = "REJECTED";
-})(TransactionStatusEnum || (TransactionStatusEnum = {}));
--- pay-process-service/dist/infrastructure/database/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-import "dotenv/config";
-const dbName = `${process.env.PUBLIC_DB_NAME}`;
-const dbUser = `${process.env.PRIVATE_DB_USER}`;
-const dbHost = `${process.env.PRIVATE_DB_HOST}`;
-export const sequelize = new Sequelize(dbName, dbUser, "", {
-    host: dbHost,
-    dialect: "mysql",
-});
